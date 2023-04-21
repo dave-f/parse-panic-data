c094914a9eadfcfb624ff7dda0c9fa4b4633524e
dave-f <dave.footitt@gmail.com>
2023-04-21 17:26:56 +0100

    WIP on decoding level data

--- main.go
+++ main.go
@@ -16,6 +16,14 @@ import (
 	"strings"
 )
 
+type Tile struct {
+	Index     int
+	FlippedX  bool
+	FlippedY  bool
+	Hookable  bool
+	Climbable bool
+}
+
 type Screen struct {
 	Tileset       int
 	StringIndex   int
@@ -28,13 +36,15 @@ type Screen struct {
 	ExitSouth     int
 	ExitEast      int
 	ExitWest      int
-
-	// another byte yet but not sure of its use
+	Data          [8][12]Tile
 }
 
 var StringTable []string
-var StringRegexp = regexp.MustCompile(`^\.s[0-9]+:`)
 var ScreenHeaders [][8]byte
+var ScreenData [][]byte
+
+var StringRegexp = regexp.MustCompile(`^\.s[0-9]+:`)
+var ScreenRegexp = regexp.MustCompile(`^\.screen[0-9]+Data`)
 
 func buildStringTable() error {
 	f, err := os.Open("../panic/memory.asm")
@@ -62,13 +72,13 @@ func buildStringTable() error {
 	return nil
 }
 
-const (
-	Undefined = iota
-	FalseBlock
-	LevelBlock
-)
-
 func buildLevelTable() error {
+
+	const (
+		Undefined = iota
+		FalseBlock
+		LevelBlock
+	)
 
 	f, err := os.Open("../panic/leveldata.asm")
 
@@ -139,6 +149,108 @@ loop:
 	return scanner.Err()
 }
 
+// Ignore items and aliens for now, maybe if the header flags suggest these we can decode them
+func buildScreenData() error {
+
+	const (
+		Undefined = iota
+		Searching
+		DecodingRow
+	)
+
+	f, err := os.Open("../panic/leveldata.asm")
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	state := Searching
+	var unpackedScreen [][8]byte
+	rowCount := 0 // pack in at 12
+
+	for scanner.Scan() {
+		l := strings.TrimSpace(scanner.Text())
+		switch state {
+		case Searching:
+			if ScreenRegexp.MatchString(scanner.Text()) {
+				fmt.Println("Decoding", scanner.Text())
+				state = DecodingRow
+			}
+		case DecodingRow:
+			if strings.HasPrefix(l, "EQUB") {
+				b, err := parseBytesFromString(l[4:])
+				if err != nil {
+					return err
+				}
+				if len(b) == 1 {
+					// If there is only one byte this row, it must be 0 to indicate no data
+					if b[0] != 0 {
+						return errors.New("unexpected byte")
+					}
+					unpackedScreen = append(unpackedScreen, [8]byte{})
+				} else {
+					// Do the decode
+					// But remember the RLE packing
+					// 0xf0 is .. repeat for rest of row
+					// 0xfn is .. repeat for n tiles
+					var unpackedRow [8]byte
+					controlByte := b[0]
+					rest := bytes.NewBuffer(b[1:])
+					runLengthCount := 0
+					var runLengthByte byte
+					idx := 0
+					for i := 0; i < 8; i++ {
+						if controlByte&0x80 == 0x80 { // Is there a tile here?
+							if runLengthCount > 0 {
+								unpackedRow[idx] = runLengthByte
+								idx++
+								runLengthCount--
+							} else {
+								tb, err := rest.ReadByte()
+								if err != nil {
+									return err
+								}
+								if tb&0xf0 == 0xf0 {
+									runLengthCount = int(tb & 0xf)
+									if runLengthCount == 0 { // 0xf0 means rest of row
+										runLengthCount = 255
+									}
+									runLengthByte, err = rest.ReadByte() // get the byte to repeat
+									if err != nil {
+										return err
+									}
+									unpackedRow[idx] = runLengthByte
+									idx++
+								} else {
+									unpackedRow[idx] = tb
+									idx++
+								}
+							}
+						} else {
+							idx++
+						}
+						controlByte = controlByte << 1
+					}
+					// unpack
+					// assert lenth of unpacked is the same?
+					//}
+					unpackedScreen = append(unpackedScreen, unpackedRow)
+				}
+				rowCount++
+				if rowCount == 12 {
+					rowCount = 0
+					state = Searching
+				}
+			}
+		}
+	}
+
+	return scanner.Err()
+}
+
 func main() {
 
 	err := buildStringTable()
@@ -160,4 +272,12 @@ func main() {
 	for i, v := range ScreenHeaders {
 		fmt.Printf("%d) %s\n", i, StringTable[v[0]&0x1f])
 	}
+
+	err = buildScreenData()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO Build proper level structure ready to marshal to XML/JSON
 }
