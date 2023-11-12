4d867030dfa56e53192851cdeaf2a5baa15cbaff
dave-f <dave.footitt@gmail.com>
2023-11-12 12:52:05 +0000

    Tidy up some byte parsing tests

--- parser_test.go
+++ parser_test.go
@@ -22,97 +22,7 @@ func TestEmptyString(t *testing.T) {
 	}
 }
 
-func TestOneByte(t *testing.T) {
-	got, _ := parseBytesFromString("255")
-	want := []byte{255}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestOneHexByteWithAmpersand(t *testing.T) {
-	got, _ := parseBytesFromString("&ff")
-	want := []byte{255}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestOneHexByteWithDollar(t *testing.T) {
-	got, _ := parseBytesFromString("$ff")
-	want := []byte{255}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestOneBinaryByte(t *testing.T) {
-	got, _ := parseBytesFromString("%11")
-	want := []byte{3}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestMultipleBytes(t *testing.T) {
-	got, _ := parseBytesFromString("1,2,3")
-	want := []byte{1, 2, 3}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestMultipleBytesWithWhitespace(t *testing.T) {
-	got, _ := parseBytesFromString(" 1, 2 ,3, 4")
-	want := []byte{1, 2, 3, 4}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestMultipleBytesWithDifferentBases(t *testing.T) {
-	got, _ := parseBytesFromString("1,&ff,$7f,%11,4")
-	want := []byte{1, 255, 127, 3, 4}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestEffectStrings(t *testing.T) {
-	got, _ := parseBytesFromString("_effectNone,_effectSnow,_effectPaletteChange,_effectPaletteChange2,_effectGems,_effectDark")
-	want := []byte{0x00, 0x10, 0x20, 0x40, 0x80, 0x08}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestScreenFlagStrings(t *testing.T) {
-	got, _ := parseBytesFromString("SCREEN_FLAGS_ITEM_PRESENT")
-	want := []byte{0x04}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func TestBytesWithOrStatements(t *testing.T) {
-	got, _ := parseBytesFromString("&01 OR &02 OR &03")
-	want := []byte{3}
-
-	if !SlicesAreEqual(got, want) {
-		t.Errorf("Expected %v got %v", want, got)
-	}
-}
-
-func NewTests(t *testing.T) {
+func TestSingleBytes(t *testing.T) {
 	type TestCase struct {
 		Input string
 		ExpectedResult byte
@@ -129,6 +39,7 @@ func NewTests(t *testing.T) {
 	tests = append(tests,TestCase{Input:"$2 or &4",ExpectedResult:6})
 	tests = append(tests,TestCase{Input:"%1",ExpectedResult:1})
 	tests = append(tests,TestCase{Input:"%1 or &2 or $4 or 8",ExpectedResult:15})
+	tests = append(tests,TestCase{Input:"SCREEN_FLAGS_ITEM_PRESENT",ExpectedResult:4})
 
 	for _,v := range tests {
 		b, err := parseByte(v.Input)
@@ -136,6 +47,30 @@ func NewTests(t *testing.T) {
 			t.Error(err)
 		}
 		if b != v.ExpectedResult {
+			t.Errorf("Expected %v got %v",v.ExpectedResult, b)
+		}
+	}
+}
+
+func TestMultipleBytes(t *testing.T) {
+
+	type TestCase struct {
+		Input string
+		ExpectedResult []byte
+	}
+
+	var tests []TestCase
+
+	tests = append(tests,TestCase{Input:"1,2,3,4",ExpectedResult:[]byte{1,2,3,4}})
+	tests = append(tests,TestCase{Input:"1,&ff,$7f,%11,4",ExpectedResult:[]byte{1,255,127,3,4}})
+	tests = append(tests,TestCase{Input:"_effectNone,_effectSnow,_effectPaletteChange,_effectPaletteChange2,_effectGems,_effectDark",ExpectedResult:[]byte{0,0x10,0x20,0x40,0x80,0x08}})
+
+	for _,v := range tests {
+		b, err := parseBytesFromString(v.Input)
+		if err != nil {
+			t.Error(err)
+		}
+		if !SlicesAreEqual(b,v.ExpectedResult) {
 			t.Errorf("Expected %v got %v",v.ExpectedResult, b)
 		}
 	}
