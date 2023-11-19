7c2cff6c478cd09cdc2595669ab8d732353d28fc
dave-f <dave.footitt@gmail.com>
2023-11-19 15:41:12 +0000

    Add extra byte test and use the builtin slices.Equal function

--- parser_test.go
+++ parser_test.go
@@ -1,18 +1,9 @@
 package main
 
-import "testing"
-
-func SlicesAreEqual(a []byte, b []byte) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
-}
+import (
+	"slices"
+	"testing"
+)
 
 func TestEmptyString(t *testing.T) {
 	got, _ := parseBytesFromString("")
@@ -24,30 +15,31 @@ func TestEmptyString(t *testing.T) {
 
 func TestSingleBytes(t *testing.T) {
 	type TestCase struct {
-		Input string
+		Input          string
 		ExpectedResult byte
 	}
 
 	var tests []TestCase
 
-	tests = append(tests,TestCase{Input:"&23 OR $4",ExpectedResult:39})
-	tests = append(tests,TestCase{Input:"&f",ExpectedResult:15})
-	tests = append(tests,TestCase{Input:"&ff",ExpectedResult:255})
-	tests = append(tests,TestCase{Input:"1 or 2 or 4",ExpectedResult:7})
-	tests = append(tests,TestCase{Input:"$2 or &4",ExpectedResult:6})
-	tests = append(tests,TestCase{Input:"23",ExpectedResult:23})
-	tests = append(tests,TestCase{Input:"$2 or &4",ExpectedResult:6})
-	tests = append(tests,TestCase{Input:"%1",ExpectedResult:1})
-	tests = append(tests,TestCase{Input:"%1 or &2 or $4 or 8",ExpectedResult:15})
-	tests = append(tests,TestCase{Input:"SCREEN_FLAGS_ITEM_PRESENT",ExpectedResult:4})
+	tests = append(tests, TestCase{Input: "&23 OR $4", ExpectedResult: 39})
+	tests = append(tests, TestCase{Input: "&f", ExpectedResult: 15})
+	tests = append(tests, TestCase{Input: "&ff", ExpectedResult: 255})
+	tests = append(tests, TestCase{Input: "1 or 2 or 4", ExpectedResult: 7})
+	tests = append(tests, TestCase{Input: "$2 or &4", ExpectedResult: 6})
+	tests = append(tests, TestCase{Input: "23", ExpectedResult: 23})
+	tests = append(tests, TestCase{Input: "$2 or &4", ExpectedResult: 6})
+	tests = append(tests, TestCase{Input: "%1", ExpectedResult: 1})
+	tests = append(tests, TestCase{Input: "%1 or &2 or $4 or 8", ExpectedResult: 15})
+	tests = append(tests, TestCase{Input: "SCREEN_FLAGS_ITEM_PRESENT", ExpectedResult: 4})
+	tests = append(tests, TestCase{Input: "1 OR 0", ExpectedResult: 1})
 
-	for _,v := range tests {
+	for _, v := range tests {
 		b, err := parseByte(v.Input)
 		if err != nil {
 			t.Error(err)
 		}
 		if b != v.ExpectedResult {
-			t.Errorf("Expected %v got %v",v.ExpectedResult, b)
+			t.Errorf("Expected %v got %v", v.ExpectedResult, b)
 		}
 	}
 }
@@ -55,23 +47,23 @@ func TestSingleBytes(t *testing.T) {
 func TestMultipleBytes(t *testing.T) {
 
 	type TestCase struct {
-		Input string
+		Input          string
 		ExpectedResult []byte
 	}
 
 	var tests []TestCase
 
-	tests = append(tests,TestCase{Input:"1,2,3,4",ExpectedResult:[]byte{1,2,3,4}})
-	tests = append(tests,TestCase{Input:"1,&ff,$7f,%11,4",ExpectedResult:[]byte{1,255,127,3,4}})
-	tests = append(tests,TestCase{Input:"_effectNone,_effectSnow,_effectPaletteChange,_effectPaletteChange2,_effectGems,_effectDark",ExpectedResult:[]byte{0,0x10,0x20,0x40,0x80,0x08}})
+	tests = append(tests, TestCase{Input: "1,2,3,4", ExpectedResult: []byte{1, 2, 3, 4}})
+	tests = append(tests, TestCase{Input: "1,&ff,$7f,%11,4", ExpectedResult: []byte{1, 255, 127, 3, 4}})
+	tests = append(tests, TestCase{Input: "_effectNone,_effectSnow,_effectPaletteChange,_effectPaletteChange2,_effectGems,_effectDark", ExpectedResult: []byte{0, 0x10, 0x20, 0x40, 0x80, 0x08}})
 
-	for _,v := range tests {
+	for _, v := range tests {
 		b, err := parseBytesFromString(v.Input)
 		if err != nil {
 			t.Error(err)
 		}
-		if !SlicesAreEqual(b,v.ExpectedResult) {
-			t.Errorf("Expected %v got %v",v.ExpectedResult, b)
+		if !slices.Equal(b, v.ExpectedResult) {
+			t.Errorf("Expected %v got %v", v.ExpectedResult, b)
 		}
 	}
 }
