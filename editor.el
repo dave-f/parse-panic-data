95cf5ccadd7bf26a23f517de6fab654302d1b80b
dave-f <dave.footitt@gmail.com>
2024-04-16 16:02:20 +0100

    More work on the Emacs level editor

--- editor.el
+++ editor.el
@@ -1,30 +1,79 @@
-;
+;; Mountain panic level editor
 
-(setq screens nil) ;
+(defvar panic-screens nil "List of screens")
+(defvar panic-current-screen 1 "Current screen we are editing")
+(defvar panic-tiles nil "A list of tile image slices")
+(defvar panic-tile-image nil "Image containing the graphics")
+(defconst panic-scale 2 "Scale of the tiles")
 
-(setq tiles nil) ; contains slices for the image
-(setq dave-im nil) ; contains the image
+;; We no longer have tilesets so just convert the tile index
+(defun tileset-index-to-tile-offset(index)
+  (if (< index 8)
+      0
+    -1))
 
-(defun create-tiles ()
-  (setq tiles nil)
+(defun panic-create-tiles()
+  (setq panic-tiles nil)
   (cl-loop for i from 0 to 7 do
-           (push `(,(* i 16) 0 16 16) tiles))
-  (setq tiles (reverse tiles)))
+           (push `(,(* i (* 16 panic-scale)) 0 ,(* 16 panic-scale) ,(* 16 panic-scale)) panic-tiles))
+  (setq panic-tiles (reverse panic-tiles)))
 
-(defun draw-level()
+(defun panic-draw-screen()
+  (when (eq panic-tile-image nil)
+    (setq panic-tile-image (create-image (expand-file-name "../panic/RES/TILE.PNG") 'png nil :scale panic-scale))
+    (message "Image loaded: %s" (image-size panic-tile-image t)))
+  (when (eq panic-tiles nil)
+    (panic-create-tiles))
+  (let ((scr (panic-create-screen))
+        (flg (panic-create-flags)))
+    (cl-loop for i from 0 below (length scr) do
+             (insert (format "%02d " i))
+             (cl-loop for j in (nth i scr) do
+                      (insert-image panic-tile-image nil nil (nth j panic-tiles)))
+             (newline))))
+
+(defun panic-create-screen()
+  (make-list 12 '(0 0 0 0 0 0 0 0)))
+
+(defun panic-create-flags()
+  (make-list 12 '(0 0 0 0 0 0 0 0)))
+
+;; Edit a cell's index, will be bound to RET
+(defun panic-edit-cell()
+  "Edit an index"
+  ;(interactive)
+  )
+
+;; Edit a cell's flags
+(defun panic-edit-cell-flags()
+  "Edit a cell flags"
+  ;(interactive)
+  (completing-read "Flags:" '("Hookable" "Climbable" "Collidable" "Flipped")))
+
+;; Edit the string table
+(defun panic-edit-string-table()
+  "Edit the string table")
+
+;; Reset the variables for debugging
+(defun panic-reset()
+  (setq panic-tiles nil)
+  (setq panic-tile-image nil))
+
+;; Main entry
+(defun panic-editor()
+  "Start editing data for Mountain Panic 2"
   (interactive)
-  (when (eq dave-im nil)
-      (setq dave-im (create-image (expand-file-name "../panic/RES/TILE.PNG") nil nil :scale 1))
-      (message "Image loaded: %s" (image-size dave-im t)))
-  (when (eq tiles nil)
-    (create-tiles))
-  (cl-loop for i from 0 to 12 do
-           (cl-loop for j from 0 to 7 do
-                    (insert-image dave-im nil nil (nth j tiles)))
-           (newline)))
+  (panic-reset)
+  (switch-to-buffer (get-buffer-create "*Panic Editor*"))
+  (erase-buffer)
+  (panic-draw-screen)
+  (message "Move around with cursor keys, RET to edit cell, s to save, q to quit, j jump to screen"))
 
+;; Load data
+(defun panic-load-data()
+  "Loads level data")
 
+;; TODO
+;; (setq level-data (json-read-file (expand-file-name "./output.json")))
+;; (length (cdr (nth 1 level-data))) ; 50 screens
 
-; TODO
-; (setq level-data (json-read-file (expand-file-name "./output.json")))
-; (length (cdr (nth 1 level-data))) ; 50 screens
